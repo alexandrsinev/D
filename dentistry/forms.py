e85076d614e9cb401757ff2eb3a3cf1aab9a3965
Alexandr Sinev <sir.sineow@yandex.ru>
2024-08-24 12:22:25 +0300

    Формы для создания и редактирования врача и услуги

---
+++ dentistry/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.forms import BooleanField
+
+from blog.models import Articles
+from dentistry.models import Doctors, Services
+
+
+class StileFormMixin():
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class DoctorsModeratorForm(StileFormMixin, forms.ModelForm):
+    class Meta:
+        model = Doctors
+        fields = ('doctors_name', 'description_doctor', 'specialization', 'photo',)
+
+
+class ServicesModeratorForm(StileFormMixin, forms.ModelForm):
+    class Meta:
+        model = Services
+        fields = ('service_name', 'description', 'photo',)
+
+
+class ArticlesModeratorForm(StileFormMixin, forms.ModelForm):
+    class Meta:
+        model = Articles
+        fields = ('title', 'contents', 'preview',)
