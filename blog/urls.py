bafe4555979cc0fba0acdf03fa25b234c647f878
Alexandr Sinev <sir.sineow@yandex.ru>
2024-08-24 12:50:54 +0300

    urls для CRUD блога

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from blog.apps import BlogConfig
+from blog.views import ArticlesListView, ArticlesCreateView, ArticlesUpdateView, ArticlesDeleteView, ArticlesDetailView
+
+app_name = BlogConfig.name
+
+urlpatterns = [
+    path('', ArticlesListView.as_view(), name='blog_list'),
+    path('blog_create/', ArticlesCreateView.as_view(), name='blog_create'),
+    path('blog_detail/<int:pk>', ArticlesDetailView.as_view(), name='blog_detail'),
+    path('blog_update/<int:pk>', ArticlesUpdateView.as_view(), name='blog_update'),
+    path('blog_delete/<int:pk>', ArticlesDeleteView.as_view(), name='blog_delete'),
+]
