1847e1e7a29dbd8977525f4d715b2ac3fa9743fc
Alexandr Sinev <sir.sineow@yandex.ru>
2024-08-24 13:58:13 +0300

    Добавлено кеширование для страниц сайта которые реже всего изменяются

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from blog.apps import BlogConfig
 from blog.views import ArticlesListView, ArticlesCreateView, ArticlesUpdateView, ArticlesDeleteView, ArticlesDetailView
@@ -6,7 +7,7 @@ from blog.views import ArticlesListView, ArticlesCreateView, ArticlesUpdateView,
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('', ArticlesListView.as_view(), name='blog_list'),
+    path('', cache_page(60)(ArticlesListView.as_view()), name='blog_list'),
     path('blog_create/', ArticlesCreateView.as_view(), name='blog_create'),
     path('blog_detail/<int:pk>', ArticlesDetailView.as_view(), name='blog_detail'),
     path('blog_update/<int:pk>', ArticlesUpdateView.as_view(), name='blog_update'),
--- dentistry/urls.py
+++ dentistry/urls.py
@@ -8,13 +8,13 @@ from dentistry.views import IndexView, AboutView, ServiceListView, ServiceCreate
 app_name = DentistryConfig.name
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='base'),
-    path('about/', AboutView.as_view(), name='about'),
-    path('services_list/', ServiceListView.as_view(), name='services'),
+    path('', cache_page(60)(IndexView.as_view()), name='base'),
+    path('about/', cache_page(60)(AboutView.as_view()), name='about'),
+    path('services_list/', cache_page(60)(ServiceListView.as_view()), name='services'),
     path('service_create/', ServiceCreateView.as_view(), name='services_create'),
     path('service_update/<int:pk>', ServiceUpdateView.as_view(), name='services_update'),
     path('service_delete/<int:pk>', ServiceDeleteView.as_view(), name='service_delete'),
-    path('doctors_list/<int:specialization_id>/', DoctorsListView.as_view(), name='doctors_list'),
+    path('doctors_list/<int:specialization_id>/', cache_page(60)(DoctorsListView.as_view()), name='doctors_list'),
     path('doctors_create/<int:specialization_id>/', DoctorCreateView.as_view(), name='doctors_create'),
     path('doctors_update/<int:specialization_id>/<int:pk>', DoctorUpdateView.as_view(), name='doctor_update'),
     path('doctors_delete/<int:specialization_id>/<int:pk>', DoctorDeleteView.as_view(), name='doctor_delete'),
