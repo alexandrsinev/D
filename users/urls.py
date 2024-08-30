a73d0b6fe97c29babd503bf2e1d1b6c788e17d62
Alexandr Sinev <sir.sineow@yandex.ru>
2024-08-31 00:11:48 +0300

    Добавлены urls для регистрации пользователя и входа в личный кабинет

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,18 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from dentistry.views import IndexView
-from users.views import StyledLoginView
+from users.views import StyledLoginView, RegisterView, ProfileView, email_verification, password_recovery, UsersView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('', StyledLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('users_update/<int:pk>', ProfileView.as_view(), name='profile'),
+    path('email-confirm/<str:token>/', email_verification, name='email-confirm'),
+    path('password_recovery/', password_recovery, name='new_password'),
+    path('users_account/', UsersView.as_view(), name='users_account'),
 ]
+
